6b797d1d772e425a7792da19c141ed532651963c
liyunhan <liyunhan11111@163.com>
2026-02-06 22:27:09 +0800

    test

---
+++ lib/page/music.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+from tkinter import messagebox
+
+class Music(tk.Toplevel):
+    def __init__(self):
+        tk.Toplevel.__init__(self)
+        self.title("音乐软件下载")
+        self.geometry("300x300")
+        self.resizable(False, False)
+        self.iconbitmap(r'..\..\res\IMG\icon.ico')
+
+if __name__ == '__main__':
+    music = Music()
+    music.mainloop()
+
--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
-from ..lib.page import music
-from ..lib.page import video
+
+import tkinter as tk
+
+class Main(tk.Tk):
+    def __init__ (self):
+        tk.Tk.__init__(self)
+        self.title("常用软件下载")
+        self.geometry("300x300")
+        self.resizable(False, False)
+        self.iconbitmap(r'..\res\IMG\DL.png')
+
+if __name__ == '__main__':
+    app = Main()
+    app.mainloop()
