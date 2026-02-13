c92734f3c8c08cfb53aebddabe49a4e4cf208e93
liyunhan <liyunhan11111@163.com>
2026-02-13 23:13:32 +0800

    test

---
+++ test/Cpp/test.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    
+    int x = 10;
+    int y = 20;
+
+    cout << x + y << endl;
+
+    return 0;
+}
+
